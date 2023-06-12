aa8183928ddc863778a0714ffab3c99c0cb034f3
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-06-12 17:39:42 +0000

    for Both(laptop&phone)devicesDiagonalScrolingDone

--- projectlist/src/App.js
+++ projectlist/src/App.js
@@ -1,21 +1,27 @@
+// for laptop devices
+
+// console.log(window.innerHeight=714+" "+window.innerWidth=1536)
 
 function App() {
-  let cnt =0;
-  let width = 380;
-  let height = 175;
+  let cnt = 0;
+  let width = 512;
+  let height = 238;
   let arr = [];
   for (let i = 0; i <= 20; i++) {
     arr[i] = i;
   }
 
-  window.addEventListener("scroll", function(e){
+  // for laptop devices
+  window.addEventListener("scroll", function (e) {
     const cards = document.getElementsByClassName("cards");
-    for(let i=0; i<cards.length; i++){
-      // cards[i].style.left = `${i*width}px`
-      console.log(window.innerHeight+" "+window.innerWidth)
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].style.transform = `translate(${
+        -Math.floor(window.scrollY) * 6.4
+      }px,
+        ${-Math.floor(window.scrollY) * 2}px)`;
       console.log(-Math.floor(window.scrollY));
     }
-  })
+  });
 
   return (
     <div className="text-4xl">
@@ -27,13 +33,14 @@ function App() {
             style={{
               width: `${width}px`,
               height: `${height}px`,
-              position:"relative",
-              left:`${width*cnt++}px`,
+              position: "relative",
+              left: `${width * cnt++}px`,
+              transform: "translate(",
               backgroundColor: `rgb(${Math.floor(
                 Math.random() * 255
               )} ${Math.floor(Math.random() * 255)} ${Math.floor(
                 Math.random() * 255
-              )})`,
+              )})`
               // top: "40px"
             }}
           >
@@ -46,3 +53,57 @@ function App() {
 }
 
 export default App;
+
+// // for phone devices
+// // console.log(window.innerHeight=714+" "+window.innerWidth=1536)
+
+// function App() {
+//   let cnt = 0;
+//   let width = 125;
+//   let height = 222;
+//   let arr = [];
+//   for (let i = 0; i <= 20; i++) {
+//     arr[i] = i;
+//   }
+
+//   window.addEventListener("scroll", function (e) {
+//     const cards = document.getElementsByClassName("cards");
+//     for (let i = 0; i < cards.length; i++) {
+//       cards[i].style.transform = `translate(${
+//         -Math.floor(window.scrollY) * 1.1
+//       }px,
+//         ${-Math.floor(window.scrollY)}px)`;
+//       console.log(-Math.floor(window.scrollY));
+//     }
+//   });
+
+//   return (
+//     <div className="text-4xl">
+//       {arr.map((el) => {
+//         return (
+//           <div
+//             key={el}
+//             className="cards"
+//             style={{
+//               width: `${width}px`,
+//               height: `${height}px`,
+//               position: "relative",
+//               left: `${width * cnt++}px`,
+//               transform: "translate(",
+//               backgroundColor: `rgb(${Math.floor(
+//                 Math.random() * 255
+//               )} ${Math.floor(Math.random() * 255)} ${Math.floor(
+//                 Math.random() * 255
+//               )})`
+//               // top: "40px"
+//             }}
+//           >
+//             hello {el}
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default App;
