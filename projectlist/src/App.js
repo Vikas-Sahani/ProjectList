54daebd757fda1fec2dd45e2d7529354c97338b2
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-06-12 12:26:34 +0000

    addEventListener is used

--- projectlist/src/App.js
+++ projectlist/src/App.js
@@ -1,35 +1,44 @@
+import { useEffect } from "react";
 
 function App() {
-  let arr=[]
-  for(let i=0; i<=20; i++){
+  let width = 150;
+  let height = 150;
+  let arr = [];
+  for (let i = 0; i <= 20; i++) {
     arr[i] = i;
   }
+
   window.addEventListener("scroll", function(e){
-    console.log(e);
+    const cards = document.getElementsByClassName("cards");
+    for(let i=0; i<cards.length; i++){
+      cards[i].style.left = `${i*width}px`
+      console.log(cards[i])
+    }
   })
+
   return (
-    <div className="text-4xl"> 
-    {arr.map(el => {
-      return (<div
-        key={el}
-        id="6"
-        style={{
-          width: "150px",
-          height: "150px",
-          backgroundColor: `rgb(${Math.floor(
-            Math.random() * 255
-          )} ${Math.floor(Math.random() * 255)} ${Math.floor(
-            Math.random() * 255
-          )})`,
-          top: "40px"
-        }}
-        onScroll={function (event) {
-          console.log(event);
-        }}
-      >
-        hello {el}
-      </div>)
-       })}
+    <div className="text-4xl">
+      {arr.map((el) => {
+        return (
+          <div
+            key={el}
+            className="cards"
+            style={{
+              width: `${width}px`,
+              height: `${height}px`,
+              position:"relative",
+              backgroundColor: `rgb(${Math.floor(
+                Math.random() * 255
+              )} ${Math.floor(Math.random() * 255)} ${Math.floor(
+                Math.random() * 255
+              )})`,
+              top: "40px"
+            }}
+          >
+            hello {el}
+          </div>
+        );
+      })}
     </div>
   );
 }
